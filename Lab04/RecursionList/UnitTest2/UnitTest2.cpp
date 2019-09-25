8b1f9ce6c1e2291e56afd52ddc8b7f1768be7aa8
Ben <32benhayden@gmail.com>
2019-09-25 14:01:34 -0400

    Added the unit test for palindrom functions

---
+++ Lab04/RecursionList/UnitTest2/UnitTest2.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\RecursionList\RecursionList.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTestPalindrome)
+	{
+	public:
+		
+		TEST_METHOD(Loop1)
+		{
+		}
+
+		TEST_METHOD(Loop2)
+		{
+		}
+
+		TEST_METHOD(Recursion1)
+		{
+		}
+
+		TEST_METHOD(Recursion2)
+		{
+		}
+
+		TEST_METHOD(Stack1)
+		{
+		}
+
+		TEST_METHOD(Stack2)
+		{
+		}
+	};
+}
