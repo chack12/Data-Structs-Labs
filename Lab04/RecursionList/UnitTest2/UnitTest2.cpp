499d58b440f15dffc3c184339d18048c0d71f8b9
Ben <32benhayden@gmail.com>
2019-09-25 18:11:55 -0400

    Finished the unit test for palindromes

--- Lab04/RecursionList/UnitTest2/UnitTest2.cpp
+++ Lab04/RecursionList/UnitTest2/UnitTest2.cpp
@@ -12,26 +12,59 @@ namespace UnitTest2
 		
 		TEST_METHOD(Loop1)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("racecar"));
+			Assert::IsFalse(checker.isPalindrome("notapalindrome"));
+			Assert::IsTrue(checker.isPalindrome("top spot"));
+			Assert::IsFalse(checker.isPalindrome("Hello, world!"));
 		}
 
 		TEST_METHOD(Loop2)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("a"));
+			Assert::IsFalse(checker.isPalindrome("ab"));
+			Assert::IsFalse(checker.isPalindrome(""));
 		}
 
 		TEST_METHOD(Recursion1)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("racecar"));
+			Assert::IsFalse(checker.isPalindrome("notapalindrome"));
+			Assert::IsTrue(checker.isPalindrome("top spot"));
+			Assert::IsFalse(checker.isPalindrome("Hello, world!"));
 		}
 
 		TEST_METHOD(Recursion2)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("a"));
+			Assert::IsFalse(checker.isPalindrome("ab"));
+			Assert::IsFalse(checker.isPalindrome(""));
 		}
 
 		TEST_METHOD(Stack1)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("racecar"));
+			Assert::IsFalse(checker.isPalindrome("notapalindrome"));
+			Assert::IsTrue(checker.isPalindrome("top spot"));
+			Assert::IsFalse(checker.isPalindrome("Hello, world!"));
 		}
 
 		TEST_METHOD(Stack2)
 		{
+			PalindromeByLoop checker;
+
+			Assert::IsTrue(checker.isPalindrome("a"));
+			Assert::IsFalse(checker.isPalindrome("ab"));
+			Assert::IsFalse(checker.isPalindrome(""));
 		}
 	};
 }
