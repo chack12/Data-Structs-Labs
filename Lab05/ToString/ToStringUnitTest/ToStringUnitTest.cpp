8ad481bff4f37588eb452d0cf0690cc9dce51c93
Christopher Hack <hackcj@mail.uc.edu>
2019-09-30 17:01:29 -0400

    Unit testing test

--- Lab05/ToString/ToStringUnitTest/ToStringUnitTest.cpp
+++ Lab05/ToString/ToStringUnitTest/ToStringUnitTest.cpp
@@ -4,43 +4,36 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace ToString
+namespace ToStringTest
 {
 	TEST_CLASS(ToStringUnitTest)
 	{
 	public:
-		
+
 		TEST_METHOD(CardClass1)
 		{
 			Deck Deck;
 
-			Card Card1 = Card("Heart", 2);
-			Card Card2 = Card("Diamond", 12);
+			Card Card1 = Card(2);
+			Card Card2 = Card(12);
 
 
 			Assert::AreEqual(2, Card1.GetRank());
 			Assert::AreEqual(12, Card1.GetRank());
 
-			Assert::AreEqual("Heart", Card1.GetSuit());
-			Assert::AreEqual("Diamond", Card2.GetSuit());
 
-			
 		}
 
 		TEST_METHOD(CardClass2)
 		{
 			Deck Deck;
 
-			Card Card1 = Card("Club", 9);
-			Card Card2 = Card("Diamond", 10);
+			Card Card1 = Card(9);
+			Card Card2 = Card(10);
 
 
 			Assert::AreEqual(9, Card1.GetRank());
 			Assert::AreEqual(10, Card1.GetRank());
-
-			Assert::AreEqual("Club", Card1.GetSuit());
-			Assert::AreEqual("Diamond", Card2.GetSuit());
-
 
 		}
 
@@ -49,21 +42,21 @@ namespace ToString
 			Deck Deck;
 
 			//Checking to make sure cards cannot be out of 1-13
-			Card Card1 = Card("Spade", 15);
+			Card Card1 = Card(15);
 			Assert::Fail();
 
 		}
 
 		TEST_METHOD(DeckClass)
 		{
-			Card Card1 = Card("Heart", 1);
+			Card* Card1 = &Card(1);
 			Deck Deck1;
 			Assert::AreEqual(Card1, Deck1.Front());
 
 
 		}
 
-		TEST_METHOD(PlayerCalss)
+		TEST_METHOD(PlayerClass)
 		{
 
 
