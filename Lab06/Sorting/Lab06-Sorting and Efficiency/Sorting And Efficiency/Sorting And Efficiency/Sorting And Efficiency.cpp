98951b1da0922fec7f93db831f314323f2f6c4fc
Christopher Hack <hackcj@mail.uc.edu>
2019-10-04 11:26:29 -0400

    Lab06 init.

---
+++ Lab06/Sorting/Lab06-Sorting and Efficiency/Sorting And Efficiency/Sorting And Efficiency/Sorting And Efficiency.cpp
@@ -0,0 +1,21 @@
+// Sorting And Efficiency.cpp : Defines the exported functions for the DLL.
+//
+
+#include "framework.h"
+#include "Sorting And Efficiency.h"
+
+
+// This is an example of an exported variable
+SORTINGANDEFFICIENCY_API int nSortingAndEfficiency=0;
+
+// This is an example of an exported function.
+SORTINGANDEFFICIENCY_API int fnSortingAndEfficiency(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+CSortingAndEfficiency::CSortingAndEfficiency()
+{
+    return;
+}
---
+++ Lab06/Sorting/Lab06-Sorting and Efficiency/Sorting And Efficiency/SortingUnitTest/SortingUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SortingUnitTest
+{
+	TEST_CLASS(SortingUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
