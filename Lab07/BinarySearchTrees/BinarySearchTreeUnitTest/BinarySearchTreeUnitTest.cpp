aecba91b14caa97999176531b64e5ed642ca6fa6
Christopher Hack <hackcj@mail.uc.edu>
2019-10-16 18:12:47 -0400

    Lab07 init.

---
+++ Lab07/BinarySearchTrees/BinarySearchTreeUnitTest/BinarySearchTreeUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BinarySearchTreeUnitTest
+{
+	TEST_CLASS(BinarySearchTreeUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ Lab07/BinarySearchTrees/BinarySearchTrees/BinarySearchTrees.cpp
@@ -0,0 +1,21 @@
+// BinarySearchTrees.cpp : Defines the exported functions for the DLL.
+//
+
+#include "framework.h"
+#include "BinarySearchTrees.h"
+
+
+// This is an example of an exported variable
+BINARYSEARCHTREES_API int nBinarySearchTrees=0;
+
+// This is an example of an exported function.
+BINARYSEARCHTREES_API int fnBinarySearchTrees(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+CBinarySearchTrees::CBinarySearchTrees()
+{
+    return;
+}
