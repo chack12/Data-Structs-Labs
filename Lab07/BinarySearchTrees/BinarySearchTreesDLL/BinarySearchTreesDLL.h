fed0aaeb60b775f987091274d06d9ab07be2b2af
Ben <32benhayden@gmail.com>
2019-10-21 15:48:24 -0400

    Created the header file

--- Lab07/BinarySearchTrees/BinarySearchTreesDLL/BinarySearchTreesDLL.h
+++ Lab07/BinarySearchTrees/BinarySearchTreesDLL/BinarySearchTreesDLL.h
@@ -10,13 +10,145 @@
 #define BINARYSEARCHTREESDLL_API __declspec(dllimport)
 #endif
 
-// This class is exported from the dll
-class BINARYSEARCHTREESDLL_API CBinarySearchTreesDLL {
+#include <iostream>
+#include <vector>
+
+//Binary Search Tree Header File
+class BINARYSEARCHTREESDLL_API BinarySearchTrees {
 public:
-	CBinarySearchTreesDLL(void);
-	// TODO: add your methods here.
+	/**
+	* The constructor for the Binary Search Tree
+	*
+	* @param
+	* @return
+	*/
+	BinarySearchTrees();
+	/**
+	* The Constructor for the Binary Search Tree
+	*
+	* @param	node	the node you want to insert
+	* @return			whether it was successful for not
+	*/
+	bool Insert(Node node);
+	/**
+	* Locates the value in the ree and returns a pointer to the node
+	* If not in the tree, returns nullptr
+	*
+	* @param	value	the value you want to find
+	* @return			whether it was successful for not
+	*/
+	Node* Find(std::string value);
+	/**
+	* Returns the size of the binary tree
+	*
+	* @param	
+	* @return			the size of the binary tree 
+	*/
+	int Size();
+	/**
+	* 
+	*
+	* @param
+	* @return
+	*/
+	std::vector<int> GetAllAscending();
+	/**
+	* 
+	*
+	* @param	
+	* @return 
+	*/
+	std::vector<int> GetAllDescending();
+	/**
+	* Checks to see if the tree is empty
+	*
+	* @param	
+	* @return	value	whether it is was successful or not 
+	*/
+	bool EmptyTree();
+	/**
+	* Removes the specified value from the binary tree
+	*
+	* @param	value	the value you want to remove
+	* @return			whether it was successful for not
+	*/
+	bool Remove(std::string value);
+	/**
+	* The destructor for the binary tree
+	*
+	* @param
+	* @return
+	*/
+	~BinarySearchTrees() {
+		delete root;
+	}
+private:
+	Node* root;
 };
 
-extern BINARYSEARCHTREESDLL_API int nBinarySearchTreesDLL;
-
-BINARYSEARCHTREESDLL_API int fnBinarySearchTreesDLL(void);
+//Binary Search Tree Node Header File
+class BINARYSEARCHTREESDLL_API Node {
+public:
+	/**
+	* The constructor for the node
+	*
+	* @param
+	* @return
+	*/
+	Node();
+	/**
+	* Sets the left pointer to the node
+	*
+	* @param	node	pointer to the left node
+	* @return	value	whether it was successful or not 
+	*/
+	bool SetLeft(Node* node);
+	/**
+	* Sets the right pointer of the node
+	*
+	* @param	node	pointer to the right
+	* @return			whether it was successful for not
+	*/
+	bool SetRight(Node* node);
+	/**
+	* Returns a pointer to the left node
+	*
+	* @param
+	* @return			the pointer to the left node
+	*/
+	Node* GetLeft();
+	/**
+	* Returns a pointer to the right node
+	*
+	* @param	
+	* @return			the pointer to the right node
+	*/
+	Node* GetRight();
+	/**
+	* Sets the item of the node
+	*
+	* @param	value	the value you want set the node to
+	* @return			whether it was successful or not
+	*/
+	bool SetItem(std::string value);
+	/**
+	* Whether or not the node is a Leaf
+	*
+	* @param	
+	* @return	value	whether it was successful or not
+	*/
+	bool IsLeaf();
+	/**
+	* The destructor for the node
+	*
+	* @param
+	* @return
+	*/
+	~Node() {
+		delete left;
+		delete right;
+	}
+private:
+	Node* left;
+	Node* right;
+};
