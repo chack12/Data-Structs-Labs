00d64bead972b2174830464db855ba93edcdce9a
Christopher Hack <hackcj@mail.uc.edu>
2019-10-31 10:31:34 -0400

    Party.h/.cpp init and EXE init.

---
+++ Lab08/TemplatesAndClassesDLL/TemplatesAndClassesDLL/Party.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+template<class ItemType>
+void Party<ItemType>::addChar(const ItemType& newCharacter) {
+
+}
---
+++ Lab08/TemplatesAndClassesDLL/TemplatesAndClassesDLL/Party.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include "ICharacter.h"
+
+#ifdef TEMPLATESANDCLASSESDLL_EXPORTS
+#define TEMPLATESANDCLASSESDLL_API __declspec(dllexport)
+#else
+#define TEMPLATESANDCLASSESDLL_API __declspec(dllimport)
+#endif
+
+template<class ItemType>
+
+class TEMPLATESANDCLASSESDLL_API Party
+{
+public:
+	/**
+	* The party constructor
+	*
+	* @param
+	* @return
+	*/
+	Party() {}
+
+	/**
+	* Adds a character to the party
+	*
+	* @param	character	char
+	* @return
+	*/
+	void addChar(const ItemType& newCharacter);
+	
+private:
+	std::vector<ItemType> frontRow;
+	std::vector<ItemType> backRow;
+};
