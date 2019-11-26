27194dda6b1c3358e32ca1e11b409a9e7d57b6f9
Christopher Hack <hackcj@mail.uc.edu>
2019-11-25 19:10:22 -0500

    Lab12 almost done?

---
+++ Lab12/environment/HelloWorld.hxx
@@ -0,0 +1,4 @@
+#include <string>
+#include "HelloWorld.hxx"
+
+std::string HelloWorld();
---
+++ Lab12/environment/Main.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "HelloWorld.cxx"
+using namespace std;
+
+
+int main () {
+    
+    cout << HelloWorld() << endl;
+    return 0;
+}
