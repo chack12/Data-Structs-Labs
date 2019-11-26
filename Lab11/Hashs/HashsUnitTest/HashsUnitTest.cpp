d8d222e1efed67d7aa3d7a397c761f3835d996f8
Ben <32benhayden@gmail.com>
2019-11-26 13:34:56 -0500

    Finsihed some of unit test

--- Lab11/Hashs/HashsUnitTest/HashsUnitTest.cpp
+++ Lab11/Hashs/HashsUnitTest/HashsUnitTest.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../HashsDLL/HashsDLL.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,7 +10,65 @@ namespace HashsUnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(AddItem1)
+		{
+			HashTable test(5);
+			Assert::IsFalse(test.addItem(7,7));
+			Assert::IsTrue(test.addItem(5,1));
+			Assert::IsTrue(test.addItem(4,2));
+			Assert::IsTrue(test.addItem(3,2));
+			Assert::IsTrue(test.addItem(2,4));
+			Assert::IsTrue(test.addItem(1,3));
+
+		}
+		TEST_METHOD(AddItem2)
+		{
+			HashTable test(5);
+			Assert::IsTrue(test.addItem(5,1));
+			Assert::IsTrue(test.addItem(4,2));
+			Assert::IsTrue(test.addItem(3,3));
+			Assert::IsTrue(test.addItem(2,4));
+			Assert::IsTrue(test.addItem(1,5));
+			Assert::IsFalse(test.addItem(0,4));
+		}
+		TEST_METHOD(RemoveItem1)
+		{
+			HashTable test(5);
+			Assert::IsTrue(test.addItem(5,1));
+			Assert::IsTrue(test.removeItem(1));
+			Assert::IsTrue(test.addItem(4,2));
+			Assert::IsTrue(test.addItem(3,5));
+			Assert::IsTrue(test.removeItem(5));
+			Assert::IsTrue(test.removeItem(2));
+		}
+		TEST_METHOD(RemoveItem2)
+		{
+			HashTable test(5);
+			Assert::IsFalse(test.removeItem(7));
+			Assert::IsFalse(test.removeItem(4));
+			Assert::IsTrue(test.addItem(5,1));
+			Assert::IsTrue(test.removeItem(1));
+			Assert::IsTrue(test.addItem(4,2));
+			Assert::IsTrue(test.removeItem(2));
+			Assert::IsFalse(test.removeItem(3));
+		}
+		TEST_METHOD(GetNode1)
+		{
+			
+		}
+		TEST_METHOD(GetNode2)
+		{
+		}
+		TEST_METHOD(GetLength1)
+		{
+		}
+		TEST_METHOD(GetLength2)
+		{
+		}
+		TEST_METHOD(Hash1)
+		{
+		}
+		TEST_METHOD(Hash2)
 		{
 		}
 	};
